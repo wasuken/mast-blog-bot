756639853fd72ba818106417da909e7510ae7d9e
wasu <wevorence@gmail.com>
2018-07-31 16:42:35 +0000

    fix bugs.

--- main.rb
+++ main.rb
@@ -7,12 +7,11 @@ require "rss"
 MYCONF=ParseConfig.new('config')
 
 url = MYCONF['blog_rss_url']
-rss = RSS::Parser.parse(url)
 TOKEN=MYCONF['mastodon_token']
 client =  Mastodon::REST::Client.new(base_url: 'https://mastodon.social', bearer_token: TOKEN)
 
 loop do
-  rss.channel.items.each do|x|
+  RSS::Parser.parse(url).channel.items.each do|x|
     client.create_status "#{i.title} \n #{i.link}"
     sleep(60 * 60)
   end
